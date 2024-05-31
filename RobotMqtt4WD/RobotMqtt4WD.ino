fb1045a4cd5f2385a53e5bd8bd457880f590a020
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2024-05-31 21:20:08 +0200

    ArduinoOTA

--- RobotMqtt4WD/RobotMqtt4WD.ino
+++ RobotMqtt4WD/RobotMqtt4WD.ino
@@ -34,11 +34,6 @@ Drive drive(espMQTTClient, statusLed);
 
 MqttClient mqttClient(espMQTTClient, drive);
 
-void onConnectionEstablished()
-{
-  mqttClient.onConnectionEstablished();
-}
-
 void setup(void)
 {
   Serial.begin(115200); // Initialising if(DEBUG)Serial Monitor
@@ -57,16 +52,19 @@ void setup(void)
   MqttPwd = configString[EConfigEEpromIdx::MqttPwdIdx];
 
   ArduinoOTA.setHostname(DeviceName.c_str());
+  ArduinoOTA.setPassword("Robot");
+  ArduinoOTA.begin();
+
   espMQTTClient.host = MqttBroker.c_str();
   espMQTTClient.username = MqttUser.c_str();
   espMQTTClient.password = MqttPwd.c_str();
   espMQTTClient.client_id = DeviceName.c_str();
-  onConnectionEstablished();
+  mqttClient.onConnectionEstablished();
   espMQTTClient.begin();
-//  espMQTTClient.setMaxPacketSize(512);
-//  espMQTTClient.enableOTA("Robot");
-//  espMQTTClient.setMqttServer(MqttBroker.c_str(), MqttUser.c_str(), MqttPwd.c_str());
-//  espMQTTClient.setMqttClientName(DeviceName.c_str());
+  //  espMQTTClient.setMaxPacketSize(512);
+  //  espMQTTClient.enableOTA("Robot");
+  //  espMQTTClient.setMqttServer(MqttBroker.c_str(), MqttUser.c_str(), MqttPwd.c_str());
+  //  espMQTTClient.setMqttClientName(DeviceName.c_str());
   //  espMQTTClient.enableDebuggingMessages(); // Enable debugging messages sent to serial output
 }
 
@@ -76,4 +74,6 @@ void loop(void)
   server.handleClient();
   drive.Poll();
   statusLed.Loop();
+  ArduinoOTA.handle();
 }
+
